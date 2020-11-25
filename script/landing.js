143668335414196d602cd0631d6e5d792e8f9822
Joseph Zhang <josephzhang@cmu.edu>
2020-11-25 12:56:24 -0800

    ball speed

--- script/landing.js
+++ script/landing.js
@@ -1,13 +1,13 @@
-var $ = document.querySelector.bind(document);
-var $on = document.addEventListener.bind(document);
-var xmouse, ymouse;
+let $ = document.querySelector.bind(document);
+let $on = document.addEventListener.bind(document);
+let xmouse, ymouse;
 $on("mousemove", function (e) {
   xmouse = e.clientX || e.pageX;
   ymouse = e.clientY || e.pageY;
 });
 
-var ball = $(".ball");
-var x = void 0,
+let ball = $(".ball");
+let x = void 0,
   y = void 0,
   dx = void 0,
   dy = void 0,
@@ -15,14 +15,14 @@ var x = void 0,
   ty = 0,
   key = -1;
 
-var followMouse = function followMouse() {
+let followMouse = function followMouse() {
   key = requestAnimationFrame(followMouse);
   if (!x || !y) {
     x = xmouse;
     y = ymouse;
   } else {
-    dx = (xmouse - x) * 0.0325;
-    dy = (ymouse - y) * 0.0325;
+    dx = (xmouse - x) * 0.0425;
+    dy = (ymouse - y) * 0.0425;
     if (Math.abs(dx) + Math.abs(dy) < 0.1) {
       x = xmouse;
       y = ymouse;
