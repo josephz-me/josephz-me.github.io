667d78df084fdf95c329743055c0980fa4316b33
Joseph Zhang <josephzhang@cmu.edu>
2020-12-02 18:16:53 -0800

    mobile interaction

--- script/imageHover.js
+++ script/imageHover.js
@@ -1,13 +1,25 @@
 jQuery(function ($) {
-  $("a").hover(
-    function (data) {
-      let className = data.currentTarget.className;
-      console.log(className);
-      $(`.${className}-video`).addClass("show");
-    },
-    function (data) {
-      let className = data.currentTarget.className;
-      $(`.${className}-video`).removeClass("show");
+  //touch screen experience
+  if ("ontouchstart" in window || !"ontouch" in window) {
+    let allVideos = document.querySelectorAll("main > video:not(.other-video)");
+    console.log(allVideos[0].className);
+
+    for (let i = 0; i < 3; i++) {
+      let desiredClass = `.${allVideos[i].className}`;
+      $(desiredClass).css("display", "none");
     }
-  );
+  }
+  //on desktop
+  else {
+    $("a").hover(
+      function (data) {
+        let className = data.currentTarget.className;
+        $(`.${className}-video`).addClass("show");
+      },
+      function (data) {
+        let className = data.currentTarget.className;
+        $(`.${className}-video`).removeClass("show");
+      }
+    );
+  }
 });
