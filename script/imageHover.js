1ae364427b8fdac80bb181639c125df806118054
Joseph Zhang <josephzhang@cmu.edu>
2020-12-09 13:10:31 -0800

    add play and pause

--- script/imageHover.js
+++ script/imageHover.js
@@ -1,7 +1,9 @@
 jQuery(function ($) {
   //touch screen experience
   if ("ontouchstart" in window || !"ontouch" in window) {
+    let desiredVideo = "other";
     let allVideos = document.querySelectorAll("main > video:not(.other-video)");
+    $(`.${desiredVideo}-video`).trigger("play");
     console.log(allVideos[0].className);
 
     for (let i = 0; i < 3; i++) {
@@ -14,10 +16,12 @@ jQuery(function ($) {
     $("a").hover(
       function (data) {
         let className = data.currentTarget.className;
+        $(`.${className}-video`).trigger("play");
         $(`.${className}-video`).addClass("show");
       },
       function (data) {
         let className = data.currentTarget.className;
+        $(`.${className}-video`).trigger("pause");
         $(`.${className}-video`).removeClass("show");
       }
     );
