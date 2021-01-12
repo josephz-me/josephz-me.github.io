5efde3892a86aefd025aaab01f960d130e4efdfb
Joseph Zhang <josephzhang@cmu.edu>
2021-01-12 15:15:38 -0800

    add expanded page

--- script/imageHover.js
+++ script/imageHover.js
@@ -1,28 +1,39 @@
 jQuery(function ($) {
   //touch screen experience
   if ("ontouchstart" in window || !"ontouch" in window) {
-    let desiredVideo = "other";
-    let allVideos = document.querySelectorAll("main > video:not(.other-video)");
+    console.log("running");
+    let desiredVideo = "thinkspace";
+    // let allVideos = document.querySelectorAll(
+    //   "main > video:not(.thinkspace-video)"
+    // );
     $(`.${desiredVideo}-video`).trigger("play");
-    console.log(allVideos[0].className);
+    // console.log(allVideos[0].className);
 
-    for (let i = 0; i < 3; i++) {
-      let desiredClass = `.${allVideos[i].className}`;
-      $(desiredClass).css("display", "none");
-    }
+    // for (let i = 0; i < 3; i++) {
+    //   let desiredClass = `.${allVideos[i].className}`;
+    //   $(desiredClass).css("display", "none");
+    // }
   }
   //on desktop
   else {
     $("a").hover(
       function (data) {
         let className = data.currentTarget.className;
-        $(`.${className}-video`).trigger("play");
-        $(`.${className}-video`).addClass("show");
+        if ($(`.${className}-video`).length === 1) {
+          $(`.${className}-video`).trigger("play");
+          $(`.${className}-video`).addClass("show");
+        } else {
+          $(`.${className}-img`).addClass("show");
+        }
       },
       function (data) {
         let className = data.currentTarget.className;
-        $(`.${className}-video`).trigger("pause");
-        $(`.${className}-video`).removeClass("show");
+        if ($(`.${className}-video`).length === 1) {
+          $(`.${className}-video`).trigger("pause");
+          $(`.${className}-video`).removeClass("show");
+        } else {
+          $(`.${className}-img`).removeClass("show");
+        }
       }
     );
   }
