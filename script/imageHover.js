a4cca1513e82ee37e0046fd31696e39f5620b8eb
Joseph Zhang <josephzhang@cmu.edu>
2021-04-16 15:21:42 -0400

    captions

--- script/imageHover.js
+++ script/imageHover.js
@@ -3,54 +3,23 @@ jQuery(function ($) {
     return;
   }
 
-  //   //on desktop
+  //on desktop
   else {
     $(document).mousemove(function (event) {});
+    let className;
+    let desiredMedia;
     $("a").hover(
       function (data) {
-        let className = data.currentTarget.className;
-        // $(".hoverCard").attr("src", `public/${className}.jpg`);
-        let desiredMedia = $(`.hoverCard.${className}`);
-        desiredMedia.addClass("showHoverCard");
+        className = data.currentTarget.className;
+        desiredMedia = $(`.${className}.hoverCard video`);
+        $(`.${className}.hoverCard`).addClass("showHoverCard");
         desiredMedia.trigger("play");
       },
-      function (data) {
-        $(".hoverCard").removeClass("showHoverCard");
+      function () {
+        $(`.${className}.hoverCard`).removeClass("showHoverCard");
+        console.log(desiredMedia);
         desiredMedia.trigger("pause");
       }
     );
   }
 });
-
-// jQuery(function ($) {
-//   //touch screen experience
-//   let fileName = location.href.split("/").slice(-1)[0];
-
-//   if ("ontouchstart" in window || !"ontouch" in window) {
-//     return;
-//   }
-//   //on desktop
-//   else {
-//     $("a").hover(
-//       function (data) {
-//         let className = data.currentTarget.className;
-
-//         if ($(`.${className}-video`).length === 1) {
-//           $(`.${className}-video`).trigger("play");
-//           $(`.${className}-video`).addClass("show");
-//         } else {
-//           $(`.${className}-img`).addClass("show");
-//         }
-//       },
-//       function (data) {
-//         let className = data.currentTarget.className;
-//         if ($(`.${className}-video`).length === 1) {
-//           $(`.${className}-video`).trigger("pause");
-//           $(`.${className}-video`).removeClass("show");
-//         } else {
-//           $(`.${className}-img`).removeClass("show");
-//         }
-//       }
-//     );
-//   }
-// });
